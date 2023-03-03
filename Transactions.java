5e31a56289b5bfff1e6f09af8f9ec54d49736314
bleuapx <123344021+bleuapx@users.noreply.github.com>
2023-03-03 22:07:31 +0000

    Combined with Account.java from slides.

--- Transactions.java
+++ Transactions.java
@@ -3,8 +3,75 @@
 // Demonstrates the creation and use of multiple Account objects.
 //*******************************************************************
 
+import java.text.NumberFormat;
+
 public class Transactions
+public class Account
 {
+   private final double RATE = 0.035; // Interest rate of 3.5%
+
+   private long acctNumber;
+   private double balance;
+   private String name;
+
+   //----------------------------------------------------------------
+   // Sets up the account by defining its owner, account number,
+   // and initial balance.
+   //----------------------------------------------------------------
+   public Account(String owner, long account, double initial)
+   {
+      name = owner;
+      acctNumber = account;
+      balance = initial;
+   }
+
+   //----------------------------------------------------------------
+   // Deposits the specified amount into the account. Returns the
+   // the new balance.
+   //----------------------------------------------------------------
+   public double deposit(double amount)
+   {
+      balance = balance + amount;
+      return balance;
+   }
+
+   //----------------------------------------------------------------
+   // Withdraws the specified amount from the account and applies
+   // the fee. Returns the new balance.
+   //----------------------------------------------------------------
+   public double withdraw(double amount, double fee)
+   {
+      balance = balance - amount - fee;
+      return balance;
+   }
+
+   //----------------------------------------------------------------
+   // Adds interest to the account and dreutns the new balance.
+   //----------------------------------------------------------------
+   public double addInterest()
+   {
+      balance += (balance * RATE);
+      return balance;
+   }
+
+   //----------------------------------------------------------------
+   // Returns the current balance of the account.
+   //----------------------------------------------------------------
+   public double getBalance;
+   {
+      return balance;
+   }
+
+   //----------------------------------------------------------------
+   // Returns a one-line description of the account as a string.
+   //----------------------------------------------------------------
+   public Striing toString()
+   {
+      NumberFormat fmt = NumberFormat.getCurrencyInstance();
+      return (acctNumber + "\t" + name + "\t" + fmt.format(balance))
+   }
+}
+
    //----------------------------------------------------------------
    // Creates some bank accounts and requests various services.
    //----------------------------------------------------------------
